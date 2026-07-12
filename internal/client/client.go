d0c354be5a29062a4cdd5cfe89e64a7afbba5520
w8wot <chris.flinn@gmail.com>
2026-07-11 19:01:21 -0500

    Add framed KV4P client

---
+++ internal/client/client.go
@@ -0,0 +1,74 @@
+package client
+
+import (
+	"io"
+
+	"github.com/w8wot/kv4p-node/internal/kiss"
+	"github.com/w8wot/kv4p-node/internal/transport"
+)
+
+type Client struct {
+	transport *transport.Transport
+	parser    *kiss.Parser
+	pending   []kiss.Frame
+}
+
+func Connect(port string) (*Client, error) {
+	t, err := transport.Open(port)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		transport: t,
+		parser:    kiss.NewParser(),
+	}, nil
+}
+
+func (c *Client) Close() error {
+	return c.transport.Close()
+}
+
+func (c *Client) Write(frame []byte) error {
+	written, err := c.transport.Write(frame)
+	if err != nil {
+		return err
+	}
+	if written != len(frame) {
+		return io.ErrShortWrite
+	}
+
+	return nil
+}
+
+func (c *Client) ReadFrame() (kiss.Frame, error) {
+	if len(c.pending) > 0 {
+		frame := c.pending[0]
+		c.pending = c.pending[1:]
+		return frame, nil
+	}
+
+	buf := make([]byte, 512)
+
+	for {
+		n, readErr := c.transport.Read(buf)
+
+		if n > 0 {
+			frames, err := c.parser.Feed(buf[:n])
+			if err != nil {
+				return kiss.Frame{}, err
+			}
+
+			if len(frames) > 0 {
+				if len(frames) > 1 {
+					c.pending = append(c.pending, frames[1:]...)
+				}
+				return frames[0], nil
+			}
+		}
+
+		if readErr != nil {
+			return kiss.Frame{}, readErr
+		}
+	}
+}
